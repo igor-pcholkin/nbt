c8f368a156ac05f4a9baa46060458e8fe3ce657b
Igor Pcholkin <igor.pcholkin@acp.aws.accenture.com>
2016-12-16 23:10:07 +0200

    introduce create command for storing project dependencies; also reading project dependencies on init

--- src/com/random/nbt/Main.scala
+++ src/com/random/nbt/Main.scala
@@ -1,14 +1,37 @@
 package com.random.nbt
 
 import java.io.File
+import com.typesafe.scalalogging.LazyLogging
+import java.io.StringWriter
+import java.io.PrintWriter
+import scala.io.Source
 
-object Main extends App {
+object Main extends App with FileUtils with LazyLogging {
   if (args.length > 0) {
-    implicit val phases = new ConfigParser().parse()
-    val phaseExecutor = new PhaseExecutor()
-    phaseExecutor.runPhase("init")
-    phaseExecutor.runPhase(args(0))
+    if (args(0) == "create")
+      createDependenciesFile(args.slice(1, args.length).mkString("").split(","))
+    else {
+      implicit val phases = new ConfigParser().parse()
+      setDependencies()
+      val phaseExecutor = new PhaseExecutor()
+      phaseExecutor.runPhase("init")
+      phaseExecutor.runPhase(args(0))
+    }
   } else {
-    println("Usage: nbt <build phase>")
+    println("Usage: nbt <build phase>| create")
+  }
+
+  def createDependenciesFile(dependencies: Array[String]) = {
+    val pw = new PrintWriter(new File("dependencies"))
+    dependencies map { dependency =>
+      pw.println(dependency)
+    }
+    pw.close()
+  }
+
+  def setDependencies() = {
+    val dependencies = Source.fromFile("dependencies").getLines().toArray
+    logger.info(s"Reading dependencies from dependencies")
+    Context.set("dependencies", dependencies)
   }
 }
