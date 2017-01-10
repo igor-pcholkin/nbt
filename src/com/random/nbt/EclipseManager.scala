c4763801242c2cb27de6de3aa565c98af65e3386
Igor Pcholkin <igor.pcholkin@acp.aws.accenture.com>
2017-01-10 08:01:34 +0200

    write formatted xml for eclipse project

--- src/com/random/nbt/EclipseManager.scala
+++ src/com/random/nbt/EclipseManager.scala
@@ -13,41 +13,42 @@ object EclipseManager {
 
   def writeProject(projectName: String) = {
     writeXML(".project",
-<projectDescription>
-  <name>{projectName}</name>
-  <comment></comment>
-  <projects>
-  </projects>
-  <buildSpec>
-    <buildCommand>
-      <name>org.scala-ide.sdt.core.scalabuilder</name>
-      <arguments>
-      </arguments>
-    </buildCommand>
-  </buildSpec>
-  <natures>
-    <nature>org.scala-ide.sdt.core.scalanature</nature>
-    <nature>org.eclipse.jdt.core.javanature</nature>
-  </natures>
-</projectDescription>
+    <projectDescription>
+      <name>{projectName}</name>
+      <comment></comment>
+      <projects>
+      </projects>
+      <buildSpec>
+        <buildCommand>
+          <name>org.scala-ide.sdt.core.scalabuilder</name>
+          <arguments>
+          </arguments>
+        </buildCommand>
+      </buildSpec>
+      <natures>
+        <nature>org.scala-ide.sdt.core.scalanature</nature>
+        <nature>org.eclipse.jdt.core.javanature</nature>
+      </natures>
+    </projectDescription>
     )
   }
 
   def writeClassPath() = {
     writeXML(".classpath",
-<classpath>
- <classpathentry kind="src" path="src"/>
- <classpathentry kind="src" path="conf"/>
- <classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER"/>
- <classpathentry kind="con" path="org.scala-ide.sdt.launching.SCALA_CONTAINER"/>
- <classpathentry kind="output" path="bin"/>
-</classpath>)
+    <classpath>
+       <classpathentry kind="src" path="src"/>
+       <classpathentry kind="src" path="conf"/>
+       <classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER"/>
+       <classpathentry kind="con" path="org.scala-ide.sdt.launching.SCALA_CONTAINER"/>
+       <classpathentry kind="output" path="bin"/>
+    </classpath>)
   }
 
   def writeXML(fileName: String, xml: Elem) = {
     val pw = new PrintWriter(new FileWriter(fileName))
     pw.println("""<?xml version="1.0" encoding="UTF-8"?>""")
-    pw.println(xml.toString)
+    val printer = new scala.xml.PrettyPrinter(80, 2)
+    pw.println(printer.format(xml))
     pw.close()
   }
 
