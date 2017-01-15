6775ba774c239f8a7c473ccb484303b71b162381
Igor Pcholkin <igor.pcholkin@acp.aws.accenture.com>
2017-01-15 23:41:46 +0200

    fix errors in eclipse project creation test

--- src/test/scala/com/random/nbt/EclipseProjectCreationTest.scala
+++ src/test/scala/com/random/nbt/EclipseProjectCreationTest.scala
@@ -3,23 +3,29 @@ package com.random.nbt
 import java.io.File
 
 import org.scalatest._
+import scala.io.Source
 
-class InternalCallHandlerTest extends FlatSpec with MustMatchers with BeforeAndAfter {
+class EclipseProjectCreationTest extends FlatSpec with MustMatchers with BeforeAndAfter {
   implicit val context = new Context
   val projectName = "Test"
   val workingDir = s"/tmp/$projectName"
 
   "InternalCallHandler" should "create Eclipse project" in {
     implicit val context = new Context
-    context.set("project", projectName)
-    context.set(Context.DEPENDENCIES, "scalaz-core.jar")
+    context.set("projectName", projectName)
+    context.set("dependenciesAsJarPaths", "scalaz-core.jar")
     context.set("currentDir", workingDir)
     CommandLineExecutor.execute(s"mkdir $workingDir")
     new InternalCallHandler("createEclipseProject").handle()
-    new File(s"$workingDir/.project").exists() mustBe true
-    new File(s"$workingDir/.classpath").exists() mustBe true
+    contains(s"$workingDir/.project", "<name>Test</name>") mustBe true
+    contains(s"$workingDir/.classpath", "scalaz-core.jar") mustBe true
     new File(s"$workingDir/src").exists() mustBe true
     new File(s"$workingDir/bin").exists() mustBe true
+  }
+
+  private def contains(fileName: String, text: String) = {
+    val fileContents = Source.fromFile(fileName).mkString
+    fileContents.contains(text)
   }
 
   before {
